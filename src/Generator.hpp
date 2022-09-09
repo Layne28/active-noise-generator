6e31dff9940bd973c8a85c5506b85a39506c0936
Layne28 <lfrechette1204@gmail.com>
2022-09-09 14:17:48 -0400

    Began implementing Generator class.

--- src/Generator.hpp
+++ src/Generator.hpp
@@ -1,20 +1,49 @@
 #ifndef GENERATOR_HPP
 #define GENERATOR_HPP
 
+#include <armadillo>
+#include <math.h>
+#include <complex>
+#include "CustomRandom.hpp"
+#include "ParamDict.hpp"
+#include "Observer.hpp"
+#include "Solver.hpp"
+
 class Generator
 {
 private:
 
 public:
+    ParamDict params;
+    arma::field<arma::cx_vec> xi_q;
+    arma::field<arma::vec> xi_r;
+    gsl_rng *rg;
 
+    double t = 0;
+    double dx = 1.0;
+    double tau = 1.0;
+    double lambda = 1.0;
+    double D = 1.0;
+
+    int nx = 10;
+    int ny = 10;
+    int nz = 10;
+
+    double Lx, Ly, Lz;
 
     /*** Methods ***/
 
     //constructor
-    Generator();
+    Generator(ParamDict &theParams, gsl_rng *&the_rg);
 
     //destructor
     ~Generator();
+
+    //take inverse fourier transform
+    void compute_r_from_q();
+
+    //Get anticorrelated gaussian random number in fourier space
+    std::complex<double> get_rnd_gauss_fourier(int i, int j, int k);
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,23 @@
 #include <iostream>
 
 #include "Generator.hpp"
+#include "CustomRandom.hpp"
 
 int main(int argc, char * argv[])
 {
-    myNoiseGen = Generator();
+
+    std::string conf_file = "sample.conf";
+    long unsigned int seed = 1;
+    if (argc>1) conf_file = argv[1];
+    if (argc>2) seed = std::atoi(argv[2]);
+    std::cout << "Using conf file: " << conf_file << std::endl;
+    std::cout << "Using seed: " << seed << std::endl;
+
+    ParamDict myParams;
+    myParams.read_params(conf_file);
+
+    gsl_rng *myRNG = CustomRandom::init_rng(seed);
+    Generator myNoiseGen(myParams, myRNG);
+
     return 0;
 }
