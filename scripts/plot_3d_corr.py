5fec03104a60cc30dd855d2ab393261129758ca7
Layne28 <lfrechette1204@gmail.com>
2022-12-22 16:47:58 -0500

    added more plotting scripts

--- scripts/plot_3d_corr.py
+++ scripts/plot_3d_corr.py
@@ -7,46 +7,7 @@ nx = int(sys.argv[2])
 lim_type = sys.argv[3]
 
 lambdas = [1.0,2.0,4.0,8.0]
-
-fig, ax = plt.subplots(4,1,sharey=True, figsize=(3.0,8.0))
-for i in range(len(lambdas)):
-    l = lambdas[i]
-    c_r = np.loadtxt('../active-noise-results/hpcc/1d/nx=%d/tau=0.01/lambda=%s/spat_corr.txt' % (nx, l))
-    dft = np.loadtxt('/home/lfrechette/research/fourier/inverse-exp_nx=%d_l=%f.txt' % (nx, l))
-    ax[i].plot(c_r[:,0],c_r[:,1]/c_r[0,1],'.-',label='simulation')
-    ax[i].plot(c_r[:,0],np.exp(-c_r[:,0]/float(l)),linewidth=0.9,label='theory')
-    ax[i].plot(dft[:,0], dft[:,1]/dft[0,1], linewidth=0.8, label='DFT', color='red')
-    ax[i].axhline(y=0, color='black', linestyle='--')
-    ax[i].set_xlabel(r'$r$')
-    ax[i].set_ylabel(r'$C(r)$')
-    ax[i].text(c_r[-1,0]*0.7, 0.8, r'$\lambda=%.00f$' % l)
-#plt.xlim([-0.1,7.1])
-#plt.ylim([-0.05,1.05])
-    if i==0:
-        ax[i].set_title(r'$\tau=0.01$')
-    if i==3:
-        ax[i].legend()
-plt.savefig(folder + '/spat_corr_1d_nx=%d_vs_lambda_tau=0.01.pdf' % nx)
-
 taus = [0.01, 0.1, 1.0, 10.0]
-fig, ax = plt.subplots(4,1,sharey=True, figsize=(3.0,8.0))
-for i in range(len(taus)):
-    tau = taus[i]
-    c_t = np.loadtxt('../active-noise-results/hpcc/1d/nx=%d/tau=%s/lambda=1.0/time_corr.txt' % (nx, tau))
-    ax[i].plot(c_t[:,0],c_t[:,1]/c_t[0,1],'.-',label='simulation')
-    ax[i].plot(c_t[:,0],np.exp(-c_t[:,0]/float(tau)),linewidth=0.9,label='theory')
-    ax[i].axhline(y=0, color='black', linestyle='--')
-    ax[i].set_xlabel(r'$t$')
-    ax[i].set_ylabel(r'$C(t)$')
-    ax[i].text(c_t[-1,0]*0.7, 0.8, r'$\tau=%.02f$' % tau)
-#plt.xlim([-0.1,7.1])
-#plt.ylim([-0.05,1.05])
-    if i==0:
-        ax[i].set_title(r'$\lambda=1$')
-    if i==3:
-        ax[i].legend()
-plt.savefig(folder + '/time_corr_1d_nx=%d_vs_tau_lambda=1.pdf' % nx)
-#plt.show()
 
 ###################################
 #Full tau vs lambda
@@ -57,8 +18,8 @@ for i in range(len(lambdas)):
     for j in range(len(taus)):
         l = lambdas[i]
         tau = taus[j]
-        c_r = np.loadtxt('../active-noise-results/hpcc/1d/nx=%d/tau=%s/lambda=%s/spat_corr.txt' % (nx, tau, l))
-        dft = np.loadtxt('/home/lfrechette/research/fourier/inverse-exp_nx=%d_l=%f.txt' % (nx, l))
+        c_r = np.loadtxt('../active-noise-results/hpcc/nx=%d_ny=%d_nz=%d/tau=%s/lambda=%s/Cx_total_nx=%d_tau=%f_lambda=%f.txt' % (nx, nx, nx, tau, l, nx, tau, l))
+        dft = np.loadtxt('/home/lfrechette/research/fourier/inverse-exp_3d_onedir_nx=%d_l=%f.txt' % (nx, l))
         ax[i,j].plot(c_r[:,0],c_r[:,1]/c_r[0,1],'.-',label='simulation')
         ax[i,j].plot(c_r[:,0],np.exp(-c_r[:,0]/float(l)),linewidth=0.9,label='theory')
         ax[i,j].plot(dft[:,0], dft[:,1]/dft[0,1], linewidth=0.8, label='DFT', color='red')
@@ -66,7 +27,7 @@ for i in range(len(lambdas)):
         if lim_type=='corr_len':
             ax[i,j].set_xlim([-0.1,0.1+3*l])
         elif lim_type=='system_size':
-            ax[i,j].set_xlim([-0.1,0.1+nx/2])
+            ax[i,j].set_xlim([-0.1,0.1+nx/2-1])
         else:
             print('error: need limit type')
             exit()
@@ -85,7 +46,7 @@ for i in range(len(lambdas)):
             ax[i,j].set_title(r'$\tau=%s$' % tau)
         if i==0 and j==0:
             ax[i,j].legend(fontsize=10)
-plt.savefig(folder + '/spat_corr_1d_nx=%d.pdf' % nx)
+plt.savefig(folder + '/spat_corr_3d_y=0_z=0_nx=%d.pdf' % nx)
 
 #time correlation
 fig, ax = plt.subplots(4,4,sharey=True, figsize=(8.0,8.0))
@@ -93,7 +54,7 @@ for i in range(len(lambdas)):
     for j in range(len(taus)):
         l = lambdas[i]
         tau = taus[j]
-        c_t = np.loadtxt('../active-noise-results/hpcc/1d/nx=%d/tau=%s/lambda=%s/time_corr.txt' % (nx, tau, l))
+        c_t = np.loadtxt('../active-noise-results/hpcc/nx=%d_ny=%d_nz=%d/tau=%s/lambda=%s/Ct_total_nx=%d_tau=%f_lambda=%f.txt' % (nx, nx, nx, tau, l, nx, tau, l))
         ax[i,j].plot(c_t[:,0],c_t[:,1]/c_t[0,1],'.-',label='simulation')
         ax[i,j].plot(c_t[:,0],np.exp(-c_t[:,0]/float(tau)),linewidth=0.9,label='theory')
         ax[i,j].axhline(y=0, color='black', linestyle='--')
@@ -112,6 +73,6 @@ for i in range(len(lambdas)):
             ax[i,j].set_title(r'$\tau=%s$' % tau)
         if i==0 and j==0:
             ax[i,j].legend(fontsize=10)
-plt.savefig(folder + '/time_corr_1d_nx=%d.pdf' % nx)
+plt.savefig(folder + '/time_corr_3d_y=0_z=0_nx=%d.pdf' % nx)
 
 #plt.show()
--- scripts/plot_corr.py
+++ scripts/plot_corr.py
@@ -3,45 +3,49 @@ import pylab as plt
 import sys
 
 folder = sys.argv[1]
+nx = int(sys.argv[2])
 
 lambdas = [1.0,2.0,4.0,8.0]
 
 fig, ax = plt.subplots(4,1,sharey=True, figsize=(3.0,8.0))
 for i in range(len(lambdas)):
     l = lambdas[i]
-    c_r = np.loadtxt('../active-noise-results/hpcc/Cr_total_nx=16_tau=0.010000_lambda=%f.txt' % l)
+    c_r = np.loadtxt('../active-noise-results/hpcc/Cr_total_nx=%d_tau=0.010000_lambda=%f.txt' % (nx, l))
+    dft = np.loadtxt('/home/lfrechette/research/fourier/inverse-exp_3d_onedir_nx=%d_l=%f.txt' % (nx, l))
     ax[i].plot(c_r[:,0],c_r[:,1],'.-',label='simulation')
     ax[i].plot(c_r[:,0],c_r[:,2],linewidth=0.9,label='theory')
+    ax[i].plot(dft[:,0],dft[:,1]/dft[0,1],linewidth=0.8,color='red',label='DFT')
     ax[i].axhline(y=0, color='black', linestyle='--')
     ax[i].set_xlabel(r'$r$')
     ax[i].set_ylabel(r'$C(r)$')
     ax[i].text(7, 0.8, r'$\lambda=%.00f$' % l)
+    ax[i].set_xlim([-0.1, nx/2.0+0.1])
 #plt.xlim([-0.1,7.1])
 #plt.ylim([-0.05,1.05])
     if i==0:
         ax[i].set_title(r'$\tau=0.01$')
     if i==3:
         ax[i].legend()
-plt.savefig(folder + '/spat_corr_vs_lambda_tau=0.01.pdf')
+plt.savefig(folder + '/spat_corr_vs_lambda_nx=%d_tau=0.01.pdf' % nx)
 
 taus = [0.01, 0.1, 1.0, 10.0]
 fig, ax = plt.subplots(4,1,sharey=True, figsize=(3.0,8.0))
 for i in range(len(taus)):
     tau = taus[i]
-    c_t = np.loadtxt('../active-noise-results/hpcc/Ct_total_nx=16_tau=%f_lambda=1.000000.txt' % tau)
+    c_t = np.loadtxt('../active-noise-results/hpcc/Ct_total_nx=%d_tau=%f_lambda=1.000000.txt' % (nx, tau))
     ax[i].plot(c_t[:,0],c_t[:,1],'.-',label='simulation')
     ax[i].plot(c_t[:,0],c_t[:,2],linewidth=0.9,label='theory')
     ax[i].axhline(y=0, color='black', linestyle='--')
     ax[i].set_xlabel(r'$t$')
     ax[i].set_ylabel(r'$C(t)$')
-    ax[i].text(c_t[-1,0]*0.8, 0.8, r'$\tau=%.00f$' % l)
+    ax[i].text(c_t[-1,0]*0.8, 0.8, r'$\tau=%.00f$' % tau)
 #plt.xlim([-0.1,7.1])
 #plt.ylim([-0.05,1.05])
     if i==0:
         ax[i].set_title(r'$\lambda=1$')
     if i==3:
         ax[i].legend()
-plt.savefig(folder + '/time_corr_vs_tau_lambda=1.pdf')
+plt.savefig(folder + '/time_corr_vs_tau_nx=%d_lambda=1.pdf' % nx)
 #plt.show()
 '''
 plt.yscale('log')
